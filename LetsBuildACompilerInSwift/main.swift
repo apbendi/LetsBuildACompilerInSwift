8f4e8362e8ff39dcf1434275b2a155a9d6d274b5
Ben DiFrancesco <ben.difrancesco@gmail.com>
2015-12-28 13:11:54 -0500

    Fake getChar with single line input for now

--- LetsBuildACompilerInSwift/main.swift
+++ LetsBuildACompilerInSwift/main.swift
@@ -1,4 +1,20 @@
 import Foundation
 
-print("// Hello")
+var inputBuffer: String! = readLine()
+var look: Character!
 
+func getChar() {
+    struct Static { static var index = 0 }
+    let i = inputBuffer.startIndex.advancedBy(Static.index)
+    Static.index
+        += 1
+    look = inputBuffer[i]
+}
+
+func start() {
+    print("// Start")
+    getChar()
+}
+
+// MARK: Main
+start()
