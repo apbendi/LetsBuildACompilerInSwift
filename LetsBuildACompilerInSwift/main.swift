6f6e09e134387a278af1726154ac4ecce8795c81
Ben DiFrancesco <ben.difrancesco@gmail.com>
2015-12-28 11:34:34 -0500

    Initial Commit

---
+++ LetsBuildACompilerInSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  LetsBuildACompilerInSwift
+//
+//  Created by Ben DiFrancesco on 12/28/15.
+//  Copyright © 2015 ScopeLift. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
