f5ef7e71046ae8b17a31dd67502b7f5253546a80
Ben DiFrancesco <ben.difrancesco@gmail.com>
2015-12-28 11:39:22 -0500

    Initial project setup

--- LetsBuildACompilerInSwift/main.swift
+++ LetsBuildACompilerInSwift/main.swift
@@ -1,12 +1,4 @@
-//
-//  main.swift
-//  LetsBuildACompilerInSwift
-//
-//  Created by Ben DiFrancesco on 12/28/15.
-//  Copyright © 2015 ScopeLift. All rights reserved.
-//
-
 import Foundation
 
-print("Hello, World!")
+print("// Hello")
 
